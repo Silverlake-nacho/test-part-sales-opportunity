262dae9ebf590443db8697b756a0295aff827df0
Silverlake-nacho <166714542+Silverlake-nacho@users.noreply.github.com>
2025-05-31 10:22:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -181,8 +181,8 @@ def index():
         if min_opportunity:
             filtered = filtered[filtered['Opportunity_Score'] >= float(min_opportunity)]
 
-         parts = filtered[['Part', 'IC Start Year', 'IC End Year', 'IC Description', 'B Price', 'Parts in Stock', 'Backorders',
-                            'Parts Sold All', 'Not Found 180 days', 'Potential_Profit', 'Sales_Speed', 'Opportunity_Score']]
+        parts = filtered[['Part', 'IC Start Year', 'IC End Year', 'IC Description', 'B Price', 'Parts in Stock', 'Backorders',
+                          'Parts Sold All', 'Not Found 180 days', 'Potential_Profit', 'Sales_Speed', 'Opportunity_Score']]
         parts = parts.sort_values(by=['Backorders', 'Opportunity_Score'], ascending=False).head(50)
         
         last_search_result = parts
